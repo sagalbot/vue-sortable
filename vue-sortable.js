486ecc7b4fff805a01b6b94d704632e637d8df4b
Taylor Zane Glaeser <tzglaeser@gmail.com>
2016-04-07 17:47:19 -0500

    Added sortable instance to VM to allow reactive updating of the sort order. (Among other things)

--- vue-sortable.js
+++ vue-sortable.js
@@ -13,9 +13,29 @@
   vSortable.config = {}
 
   vSortable.install = function (Vue) {
+    var vSortableIdCounter = 0
+
     Vue.directive('sortable', function (options) {
       options = options || {}
+
+      var vSortableId = options.vSortableId || vSortableIdCounter++
+
+      if (!this.vm.vSortable) {
+        this.vm.vSortable = {}
+      }
+
+      // Only throw an error if the user specified the ID, don't fault the user
+      // if they have custom ID's 0 - 10, and the counter is between 0 & 10.
+      if (options.vSortableId && this.vm.vSortable[vSortableId]) {
+        throw new Error('[vue-sortable] cannot set already defined id: \'' + vSortableId + '\'')
+      }
+
+      while(this.vm.vSortable[vSortableId]) {
+        vSortableId = vSortableIdCounter++
+      }
+
       this.sortable = new Sortable(this.el, options)
+      this.vm.vSortable[vSortableId] = this.sortable
     })
   }
 

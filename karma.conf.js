762406187791c3c2c945fdaab03dd18fc17a99e6
Jeff Sagal <sagalbot@gmail.com>
2016-04-27 20:55:29 -0700

    travis needs --single-run or it fails

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     preprocessors: {
       'test/*.js': ['webpack']
     },
+    singleRun: true,
 
     webpack: {
       // karma watches the test entry points

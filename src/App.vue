c308108f9a90a39b886a6e11368c2d4265f34162
Jeff <sagalbot@gmail.com>
2020-08-21 08:41:59 -0700

    Vue 3 base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Sortable tag="ul">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+    <li>Four</li>
+  </Sortable>
+</template>
+
+<script>
+import Sortable from "./components/Sortable.vue";
+export default {
+  name: "App",
+  components: { Sortable },
+};
+</script>
